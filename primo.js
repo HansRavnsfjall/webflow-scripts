9010a0894a5880562fe2489213fbec358546c1e2
HansRavnsfjall <hans.ravnsfjall@gmail.com>
2025-03-20 09:59:46 +0000

    Update primo.js

--- primo.js
+++ primo.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
   let loadTl = gsap.timeline();
   loadTl.from(".intro-title-1", { opacity: 0, y: "6rem", duration: 1 });
   loadTl.from(".intro-title-2", { opacity: 0, y: "6rem", duration: 1 });
-  loadTl.from(".intro-bread", { opacity: 0, scale: 0.8, duration: 2 });
-  loadTl.from(".intro-cta", { opacity: 0, scale: 0.8, duration: 2 });
+  loadTl.from(".intro-bread", { opacity: 0, scale: 0.8, duration: 1 });
+  loadTl.from(".intro-cta", { opacity: 0,  duration: 1 });
 });
